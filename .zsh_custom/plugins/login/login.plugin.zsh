61e8ffecc91b254ffcdf0579f0d2a9d0bddd0772
zhoumingjun <zhoumingjun@sensetime.com>
2020-01-21 11:10:22 +0800

    update

--- .zsh_custom/plugins/login/login.plugin.zsh
+++ .zsh_custom/plugins/login/login.plugin.zsh
@@ -1,3 +1,6 @@
+if [[ -e "$HOME/.zsh_custom/plugins/login/gauth.sh" ]] ; then
+  source "$HOME/.zsh_custom/plugins/login/gauth.sh" 
+fi
 if [[ -e "$HOME/.zsh_custom/plugins/login/login.sh" ]] ; then
   alias lg="$HOME/.zsh_custom/plugins/login/login.sh"
 fi
