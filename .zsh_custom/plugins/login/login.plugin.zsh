8bf7d4a97c59b2d8acb096637a1a4c2c32930a2b
zhoumingjun <zhoumingjun@gmail.com>
2020-02-13 02:18:30 +0800

    update

--- .zsh_custom/plugins/login/login.plugin.zsh
+++ .zsh_custom/plugins/login/login.plugin.zsh
@@ -3,3 +3,8 @@
 if [[ -e "$HOME/.zsh_custom/plugins/login/login.zsh" ]] ; then
   alias lg="$HOME/.zsh_custom/plugins/login/login.zsh"
 fi
+
+if [[ -e "$HOME/.zsh_custom/plugins/login/login-proxy.zsh" ]] ; then
+  alias lgp="$HOME/.zsh_custom/plugins/login/login-proxy.zsh"
+fi
+
