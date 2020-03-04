d742d0a7f86f686d7aaebf609765482b0f8ce6d6
zhoumingjun <zhoumingjun@sensetime.com>
2020-03-04 12:47:32 +0800

    update

--- .zsh_custom/plugins/proxy/proxy.plugin.zsh
+++ .zsh_custom/plugins/proxy/proxy.plugin.zsh
@@ -1,7 +1,10 @@
 alias tps="export http_proxy=socks5://localhost:1080;export https_proxy=socks5://localhost:1080"
 alias tph="export http_proxy=http://localhost:1081;export https_proxy=http://localhost:1081"
 
-alias tps1="export http_proxy=socks5://10.107.1.195:1080;export https_proxy=socks5://10.107.1.195:1080"
-alias tph1="export http_proxy=http://10.107.1.195:1081;export https_proxy=http://10.107.1.195:1081"
+alias tps-st1="export http_proxy=socks5://10.107.1.195:1080;export https_proxy=socks5://10.107.1.195:1080"
+alias tph-st1="export http_proxy=http://10.107.1.195:1081;export https_proxy=http://10.107.1.195:1081"
+
+alias tph-st2="export http_proxy=http://10.54.0.22:3128;export https_proxy=http://10.54.0.22:3128"
+
 
 alias tpu="unset http_proxy; unset https_proxy"
