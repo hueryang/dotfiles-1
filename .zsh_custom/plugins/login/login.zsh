d0bd7f779d83eed5078e119789d79386a6d612c3
zhoumingjun <zhoumingjun@gmail.com>
2020-03-29 12:18:09 +0800

    update

--- .zsh_custom/plugins/login/login.zsh
+++ .zsh_custom/plugins/login/login.zsh
@@ -1,19 +1,26 @@
 #!/usr/bin/env zsh
 
-verificationcode=`oathtool --totp --base32 -d6 WASBPQMYOM4HI2W5`
+if [[ ! -e "$HOME/.secret/$1" ]] ; then
+    echo "$HOME/.secret/$1 does not exist"
+    exit 1
+fi
+
+source "$HOME/.secret/$1" 
+
+verificationcode=`oathtool --totp --base32 -d6 $OATH_SECRET` 
 
 
 expect <(cat <<EOD
-spawn ssh jump01.bj.sensetime.com 
+spawn ssh $JUMPER_SERVER 
 
 expect  "*Verification code:" 
 send "$verificationcode\r"  
 
 expect  "*Password:" 
-send "Passw0rd@123\r" 
+send "$AD_PASSWORD\r" 
 
 expect "*Please enter your Login Ip:" 
-send "$1\r" 
+send "$2\r" 
 interact
 
 EOD
