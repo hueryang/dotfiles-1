25444aa933b163f5aabcd2b5b957ffee64cbc34b
zhoumingjun <zhoumingjun@gmail.com>
2020-10-30 08:19:17 +0800

    update

--- .zsh_custom/plugins/login/login.zsh
+++ .zsh_custom/plugins/login/login.zsh
@@ -13,13 +13,14 @@ code=`oathtool --totp --base32 -d6 $OATH_SECRET`
 expect <(cat <<EOD
 spawn ssh $JUMPER_SERVER 
 
-expect  "*Verification code:" 
-send "$code\r"  
-
-expect  "*Password:" 
+expect  "*password:" 
 send "$AD_SECRET\r" 
 
-expect "*Please enter your Login Ip:" 
+expect "*auth]:"
+send "$code\r"
+
+
+expect "Opt>" 
 send "$2\r" 
 interact
 
