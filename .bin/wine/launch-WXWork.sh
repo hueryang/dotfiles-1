de44a6274a550fa65b08b181668e820d76922ffa
zhoumingjun <zhoumingjun@gmail.com>
2020-12-23 22:07:38 +0800

    update

--- .bin/wine/launch-WXWork.sh
+++ .bin/wine/launch-WXWork.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /work/wines/office/drive_c/apps/WXWork
-LANG=zh_CN.UTF-8 WINEPREFIX="/work/wines/office" WINEARCH=win32 wine start WXWork.exe
+#LANG=zh_CN.UTF-8 WINEPREFIX="/work/wines/office" WINEARCH=win32 wine start WXWork.exe
+LANG=zh_CN.UTF-8 WINEPREFIX="/work/wines/office" WINEARCH=win32 wine explorer /desktop=wxwork,1080x700 WXWork.exe
--- .bin/wine/launch-WeChat.sh
+++ .bin/wine/launch-WeChat.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /work/wines/office/drive_c/apps/WeChat/
-LANG=zh_CN.UTF-8 WINEPREFIX="/work/wines/office" WINEARCH=win32 wine start WeChat.exe
+#LANG=zh_CN.UTF-8 WINEPREFIX="/work/wines/office" WINEARCH=win32 wine start WeChat.exe
+LANG=zh_CN.UTF-8 WINEPREFIX="/work/wines/office" WINEARCH=win32 wine explorer /desktop:wechat,1080x700 WeChat.exe
