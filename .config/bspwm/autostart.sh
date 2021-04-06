d90749d4fc107f1d67b01f3b910ee716cb3e14c9
zhoumingjun <zhoumingjun@gmail.com>
2021-04-06 15:54:13 +0800

    update

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -4,6 +4,8 @@ command -v dunstk &> /dev/null &&  dunst --config $HOME/.config/dunst/dunstrc &
 command -v picom &> /dev/null && picom --config $HOME/.config/picom/picom.conf &
 command -v xautolock &> /dev/null && xautolock -time 10 -locker "i3lock -c 000000" &
 
+pgrep -x sxhkd > /dev/null || sxhkd &
+
 $HOME/.config/polybar/launch.sh &
 
 
