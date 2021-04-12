44c37cd753bb4824d8793e06442de30ff2d3ee9e
zhoumingjun <zhoumingjun@gmail.com>
2021-04-12 23:23:14 +0800

    update

--- .config/bspwm/monitor.sh
+++ .config/bspwm/monitor.sh
@@ -9,6 +9,7 @@ then
     bspc monitor -d 1 2 3 4 5 6 7 8 9 0
 elif [ ${#monitors[@]} -eq 2 ]
 then
+    $HOME/.bin/office-desktop.sh
     echo "monitor number is 2" >> ~/log
     bspc monitor ${monitors[0]} -d 1 2 3 4 5
     bspc monitor ${monitors[1]} -d 6 7 8 9 0
