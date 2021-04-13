483ad04c99eebbe2f02a91992f006243fa56119a
zhoumingjun <zhoumingjun@gmail.com>
2021-04-13 17:08:53 +0800

    update

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -13,25 +13,5 @@ pgrep -x sxhkd > /dev/null || sxhkd &
 # launch polybar
 $HOME/.config/polybar/launch.sh &
 
-if [ -x "$(command -v fcitx5)" ]; then
-    echo 'use fcitx5'
-    export GTK_IM_MODULE=fcitx5
-    export QT_IM_MODULE=fcitx5
-    export XMODIFIERS=@im=fcitx5
-    fcitx5 &  
-elif [ -x "$(command -v fcitx)" ]; then
-    echo 'use fcitx'
-    export GTK_IM_MODULE=fcitx
-    export QT_IM_MODULE=fcitx
-    export XMODIFIERS=@im=fcitx
-    fcitx -dr    
-elif [ -x "$(command -v ibus-daemon)" ]; then
-    echo 'use ibus'
-    export GTK_IM_MODULE=ibus
-    export QT_IM_MODULE=ibus
-    export XMODIFIERS=@im=ibus
-    ibus-daemon -drx
-fi
-
 
 
