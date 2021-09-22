dc31483647a32a29dd97d7f909aa3e8d130b61a1
zhoumingjun <zhoumingjun@sensetime.com>
2021-09-22 15:31:14 +0800

    update

--- .bin/office-desktop.sh
+++ .bin/office-desktop.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output DP1 --primary --mode 1920x1080 --pos 0x184 --rotate normal --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate left  --output HDMI3 --off  --output VIRTUAL1 --off
+xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x184 --rotate normal \
+	--output HDMI-1 --off \
+	--output HDMI-2 --mode 1920x1080 --pos 1920x0 --rotate left  \
+	--output HDMI-3 --off  
